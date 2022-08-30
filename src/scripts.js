582b3a7ddb3e744bd79d7ebc64e37a9d409184d2
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2022-08-29 20:27:19 -0500

    Add query selectors and global variables to js

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,42 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-console.log(userData,"<>>>>userData")
-// An example of how you tell webpack to use a CSS file
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
-
-// An example of how you tell webpack to use a JS file
-
-import userData from './data/users';
-
+import User from './User';
+import userData from './data/userData.js';
 import UserRepository from './UserRepository';
+
+
+// --------------------------------------------------- QUERY SELECTORS
+const welcomeUserBox = document.getElementById('welcomeUser-box');
+const userDataBox = document.getElementById('userData-box');
+const activityBox = document.getElementById('activity-Box');
+const sleepBox = document.getElementById('sleep-box');
+const hydrationBox = document.getElementById('hydration-box');
+const communityBox = document.getElementById('community-box');
+
+// --------------------------------------------------- GLOBAL VARIABLES
+let userRepository = new UserRepository(); // <------ may need parameter
+let user, randomUser;
+let users = [];
+
+// --------------------------------------------------- EVENT LISTENERS
+
+
+// --------------------------------------------------- FUNCTIONS
+function getRandomId(userList) {
+    return Math.floor(Math.random() * userList.length);
+}
+
+function displayUserData() {
+    randomUser = getRandomId(userRepository.users);
+    user = userRepository.users[randomUser];
+    displayPageInfo();
+}
+
+function displayPageInfo() {
+    userDataBox.innerText = `${user.name}`;
+    console.log('ayyyyy', user.name);
+}
+// **THINK TANK**
+// going to need a function that gets all the userData
+// change user button later....maybe??? 
+// get first name
