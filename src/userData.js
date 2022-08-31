5d65b6f8e51ef1692c0ca2bd0e6ebd9844a57c8a
Connor Corcoran <connorcorc@gmail.com>
2022-08-31 16:19:44 -0600

    Add hydration and sleep data to userData

--- src/userData.js
+++ src/userData.js
@@ -42,4 +42,46 @@ const userData = [
     }
 ];
 
+const hydrationData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
+  }
+]
+
+const sleepData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  }
+]
+
+
 module.exports = { userData };
+module.exports = { hydrationData };
+module.exports = { sleepData }
