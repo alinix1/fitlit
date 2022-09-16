a120450657353084f13203ea80848b11f118a2ac
Connor Corcoran <connorcorc@gmail.com>
2022-09-15 21:35:08 -0600

    Add full functionality and testing for the activity class

--- src/Activity.js
+++ src/Activity.js
@@ -1,8 +1,9 @@
-import { DatasetController } from "chart.js";
+// import { DatasetController } from "chart.js";
 
 class Activity {
     constructor(activityData) {
-        this.activityData = activityData;    
+        this.activityData = activityData;
+
     }
 
     getUserById(id) {
@@ -22,56 +23,98 @@ class Activity {
       }
 
       getMinActiveByDate(id) {
-         const activityById = this.getUserById(id)
-         const getDate = activityById.map(userData => userData.date).pop()
-         const activityByDate = activityById.find(userData => userData.date === getDate)
-         return activityByDate.minutesActive
-      
+        const activityById = this.getUserById(id)
+        const getDate = activityById.map(activity => activity.date).shift()
+        const activityByDate = activityById.find(activity => activity.date === getDate)
+        return activityByDate.minutesActive
+      }
+
+        getAverageActivityByDate(date) {
+          const byDate = this.activityData.filter(user => user.date === date);
+          const avgActivity = byDate.reduce((acc, user) => {
+            acc += user.minutesActive;
+            return acc;
+          }, 0) / byDate.length
+          console.log(Math.round(avgActivity))
+          return Math.round(avgActivity)
         }
 
-        getAverageActivityByDate(id, date) {
-          const activityById = this.getUserById(id)
-          const avgActivity = activityById.filter(date => data.date === date)
-          .reduce((acc, data) => {
-            acc += data.minutesActive
-            return acc
-          }, 0) / activityById.length 
-          return avgActivity.toFixed(0)
+        getAverageStepsByDate(date) {
+          const byDate = this.activityData.filter(user => user.date === date);
+          const avgSteps = byDate.reduce((acc, user) => {
+            acc += user.numSteps;
+            return acc;
+          }, 0) / byDate.length
+          return Math.round(avgSteps)
         }
 
-        getAverageStepsByDate(id, date) {
-          const activityById = this.getUserById(id)
-          const avgActivity = activityById.filter(date => data.date === date)
-          .reduce((acc, data) => {
-            acc += data.numSteps
-            return acc
-          }, 0) / activityById.length 
-          return avgActivity.toFixed(0)
+        getAverageStairsByDate(date) {
+          const byDate = this.activityData.filter(user => user.date === date);
+          const avgStairs = byDate.reduce((acc, user) => {
+            acc += user.flightsOfStairs;
+            return acc;
+          }, 0) / byDate.length
+          return Math.round(avgStairs)
         }
 
-        getAverageStairsByDate(id, date) {
+        getMilesWalked(id, user, date) {
+          const keys = Object.keys(user)
           const activityById = this.getUserById(id)
-          const avgActivity = activityById.filter(date => data.date === date)
-          .reduce((acc, data) => {
-            acc += data.flightsOfStairs
+          const milesWalked = activityById.reduce((acc, currentUser) => {
+            user[keys].filter(use => {
+              if(currentUser.userID === use.id) {
+                acc = (use.strideLength * currentUser.numSteps) / 5280
+              }
+            })
+            return acc;
+          }, 0)
+          console.log(Math.round(milesWalked*2)/2)
+          return Math.round(milesWalked*2)/2;
+        }
+
+        reachStepGoal(id, user, date) {
+          const keys = Object.keys(user)
+          const activityById = this.getUserById(id)
+          const compareSteps = activityById.reduce((acc, currentUser) => {
+            let newUser = user[keys].filter(use => {
+              if(currentUser.userID === use.id) {
+                if (currentUser.numSteps >= use.dailyStepGoal) {
+                  acc = true
+                } else {
+                  acc = false
+                }
+              }
+            })
             return acc
-          }, 0) / activityById.length 
-          return avgActivity.toFixed(0)
+          })
+          return compareSteps
+        }
+
+        reachStepGoalByDate(id, user, date) {
+          const keys = Object.keys(user)
+          const activityById = this.getUserById(id)
+          const compareSteps = activityById.reduce((acc, currentUser) => {
+            let newUser = user[keys].filter(use => {
+              if(currentUser.userID === use.id && date === currentUser.date) {
+                if (currentUser.numSteps >= use.dailyStepGoal) {
+                  acc.push(currentUser.date);
+                }
+              }
+            })
+            return acc
+          }, [])
+          console.log(compareSteps)
+          return compareSteps
         }
 
         getActivityAvgWeek(id) {
           const activityById = this.getUserById(id).splice(0, 7)
           const activeWeek = activityById.reduce((acc, currentUser) => {
-            acc[currentUser.date] = currentUser.minutesActive
+            acc += currentUser.minutesActive
             return acc
-          }, {})
-          // }, {}) // need the average // return a num instead of an object?
+          }, 0) / activityById.length
           return activeWeek
         }
       }
 
 export default Activity
-
-// way to DRY up the above averages for all users - mins active, steps, stairs
-// similar to hours slept and quality of sleep methods 
-
